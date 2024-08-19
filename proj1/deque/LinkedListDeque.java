6b4d6a18e7ae53393108a28e148f07e03c05d834
XDuan <xinxin.duanxx@gmail.com>
2024-08-19 15:31:38 +0200

    Start project 1 skeleton

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,64 @@
+package deque;
+
+public class LinkedListDeque<T> {
+    private class ItemNode {
+        public T item;
+        public ItemNode previous;
+        public ItemNode next;
+
+        public ItemNode(T i, ItemNode p, ItemNode n) {
+            item = i;
+            previous = p;
+            next = n;
+        }
+    }
+
+    private int size;
+    private ItemNode sentinel;
+
+    public LinkedListDeque() {
+        sentinel = new ItemNode(null, null, null);
+        size = 0;
+    }
+    /** Adds an item of type T to the front of the deque. */
+    public void addFirst(T item) {
+        sentinel.next = new ItemNode(item, sentinel, sentinel);
+        size += 1;
+    }
+
+    /** Adds an item of type T to the back of the deque. */
+    public void addLast(T item) {
+
+    }
+
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty() {
+
+    }
+
+    /** Returns the number of items in the deque. */
+    public int size() {
+        return size;
+    }
+    /** Prints the items in the deque from first to last, separated by a space.
+     * Once all the items have been printed, print out a new line. */
+    public void printDeque() {
+
+    }
+
+    /** Removes and returns the item at the front of the deque. If no such item exists, returns null. */
+    public T removeFirst() {
+
+    }
+
+    /** Removes and returns the item at the back of the deque. If no such item exists, returns null. */
+    public T removeLast() {
+
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque! */
+    public T get(int index) {
+
+    }
+}
