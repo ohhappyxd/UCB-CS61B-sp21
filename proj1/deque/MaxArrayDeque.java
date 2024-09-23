bf1676ffee7743c10bd92f4561e35d07bfa53bec
XDuan <xinxin.duanxx@gmail.com>
2024-09-23 22:03:02 +0200

    Create MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,4 +1,24 @@
 package deque;
 
+import java.util.Comparator;
+
 public class MaxArrayDeque<T> extends ArrayDeque<T>{
+    public Comparator<T> defaultComparator;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        defaultComparator = c;
+    }
+
+    /** Returns the maximum element in the deque as governed by the previously given Comparator.
+     * If the MaxArrayDeque is empty, returns null.*/
+    public T max() {
+        return null;
+    }
+
+    /** Returns the maximum element in the deque as governed by the parameter Comparator c.
+     * If the MaxArrayDeque is empty, returns null.*/
+    public T max(Comparator<T> c) {
+        return null;
+    }
 }
