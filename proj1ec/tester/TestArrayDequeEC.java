4450dde20d68ebce37a740ea91e1a5a3fce1fca6
XDuan <xinxin.duanxx@gmail.com>
2024-10-03 10:37:04 +0200

    Finished Task II of TestArrayDequeEC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -11,6 +11,7 @@ public class TestArrayDequeEC {
     public void testSAD() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solution1 = new ArrayDequeSolution<>();
+        String message= "";
 
         for (int i = 0; i < 10000; i += 1) {
             double randomNumber = StdRandom.uniform(0.0, 4.0);
@@ -18,20 +19,24 @@ public class TestArrayDequeEC {
             if (randomNumber < 1.0) {
                 sad1.addLast(i);
                 solution1.addLast(i);
+                message = message + "addLast(" + i + ")\n";
             } else if (randomNumber < 2.0) {
                 sad1.addFirst(i);
                 solution1.addFirst(i);
+                message = message + "addFirst(" + i + ")\n";
             } else if (randomNumber < 3.0) {
                 if (!sad1.isEmpty()) {
                     Integer student = sad1.removeLast();
                     Integer solution = solution1.removeLast();
-                    assertEquals(student, solution);
+                    message = message + "removeLast()\n";
+                    assertEquals(message, student, solution);
                 }
             } else {
                 if (!sad1.isEmpty()) {
                     Integer student = sad1.removeFirst();
                     Integer solution = solution1.removeFirst();
-                    assertEquals(student, solution);
+                    message = message + "removeFirst()\n";
+                    assertEquals(message, student, solution);
                 }
             }
         }
